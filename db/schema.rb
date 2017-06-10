52f24e78e92368ba450f59edda84ad28d7d3a80a
meghal <meghalbulsara22@gmail.com>
2017-06-10 17:29:32 +0530

    Added advertisement

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610113657) do
+ActiveRecord::Schema.define(version: 20170610115551) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
