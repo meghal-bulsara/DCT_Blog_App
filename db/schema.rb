daa8c7007d856c3c1756f364f2d919d67bf8ef69
meghal <meghalbulsara22@gmail.com>
2017-06-10 17:49:36 +0530

    Subscriber added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610115551) do
+ActiveRecord::Schema.define(version: 20170610121643) do
 
   create_table "advertisements", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -42,6 +42,12 @@ ActiveRecord::Schema.define(version: 20170610115551) do
     t.string   "title"
     t.text     "body"
     t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subscribers", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
